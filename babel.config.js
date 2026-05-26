b1b8b94ee80d34dbce9a16c67637668c161f94be
Emerson Silva <emerson_jdss@hotmail.com>
2026-05-26 01:09:39 -0300

    build: configura aliases e fronteiras de import FSD-lite

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,25 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js", ".jsx"],
+          alias: {
+            "@shared": "./src/shared",
+            "@entities": "./src/entities",
+            "@features": "./src/features",
+            "@widgets": "./src/widgets",
+            "@app": "./app",
+            "@ui": "./src/shared/ui",
+            "@lib": "./src/shared/lib",
+            "@theme": "./src/shared/theme",
+            "@types": "./src/types",
+          },
+        },
+      ],
+    ],
   };
 };
