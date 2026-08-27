bdb8db701640aeb27fda42b9d62553f29d6c68cd
Emerson Silva <emerson_jdss@hotmail.com>
2026-08-27 19:16:00 -0300

    test(workout): cover session persistence end to end with msw

--- tests/msw/handlers.ts
+++ tests/msw/handlers.ts
@@ -1,3 +1,4 @@
 import { authHandlers } from "./auth-handlers";
+import { sessionHandlers } from "./session-handlers";
 
-export const handlers = [...authHandlers];
+export const handlers = [...authHandlers, ...sessionHandlers];
